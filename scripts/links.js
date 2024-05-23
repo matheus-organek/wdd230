484c39bdeea6370c8a7c515bea72b22f24fd2039
matheus-organek <matheusorganek@icloud.com>
2024-05-22 22:09:18 -0300

    homepage finished

--- scripts/links.js
+++ scripts/links.js
@@ -18,12 +18,19 @@ function displayLinks(weeks) {
         weekSection.appendChild(weekTitle);
 
         const linkList = document.createElement('ul');
-        week.links.forEach(link => {
+        week.links.forEach((link, index) => {
             const listItem = document.createElement('li');
             const anchor = document.createElement('a');
             anchor.href = baseURL + link.url;
             anchor.textContent = link.title;
             listItem.appendChild(anchor);
+
+            
+            if (index < week.links.length - 1) {
+                const separator = document.createTextNode(' | ');
+                listItem.appendChild(separator);
+            }
+
             linkList.appendChild(listItem);
         });
 
