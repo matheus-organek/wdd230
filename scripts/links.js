ca2616c2290452c0701ae23afe3fea0660f70257
matheus-organek <matheusorganek@icloud.com>
2024-05-22 21:37:37 -0300

    style uptade

--- scripts/links.js
+++ scripts/links.js
@@ -1,1 +1,10 @@
 const baseURL = "https://matheus-organek.github.io/wdd230/";
+const linksURL = "https://matheus-organek.github.io/wdd230/data/links.json";
+
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    displayLinks(data);
+}
+
+getLinks();
